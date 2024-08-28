01ddfeb01dbf42b460bb89ee3310025038090d7e
libaice <libaice147@gmail.com>
2024-08-28 15:02:56 +0400

    init counter deployment

---
+++ scripts/deployCounter.ts
@@ -0,0 +1,12 @@
+import { toNano } from '@ton/core';
+import { Counter } from '../wrappers/Counter';
+import { compile, NetworkProvider } from '@ton/blueprint';
+
+export async function run(provider: NetworkProvider) {
+    const counter = provider.open(Counter.createFromConfig({}, await compile('Counter')));
+
+    await counter.sendDeploy(provider.sender(), toNano('0.01'));
+
+    await provider.waitForDeploy(counter.address);
+
+}
