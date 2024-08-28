a99924f388335ded9ae70e932090e46b9dc1f06f
libaice <libaice147@gmail.com>
2024-08-28 16:19:36 +0400

    update counter test

--- scripts/sendNumber.ts
+++ scripts/sendNumber.ts
@@ -1,11 +1,16 @@
 import { toNano } from '@ton/core';
 import { Counter } from '../wrappers/Counter';
-import { compile, NetworkProvider } from '@ton/blueprint';
+import {compile, NetworkProvider, sleep} from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
     const counter = provider.open(Counter.createFromConfig({}, await compile('Counter')));
-
-    // await counter.sendNumber(provider.sender(), toNano('0.005'), 100n);
     console.log('Total:', await counter.getTotal());
+    // await counter.sendNumber(provider.sender(), toNano('0.005'), 100n);
+
+    // for(let i = 0; i < 10; i++) {
+    //     console.log('Total:', await counter.getTotal());
+    //     await sleep(5000);
+    // }
+
 
 }
--- tests/Counter.spec.ts
+++ tests/Counter.spec.ts
@@ -1,8 +1,8 @@
-import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Cell, toNano } from '@ton/core';
-import { Counter } from '../wrappers/Counter';
+import {Blockchain, SandboxContract, TreasuryContract} from '@ton/sandbox';
+import {Cell, toNano} from '@ton/core';
+import {Counter} from '../wrappers/Counter';
 import '@ton/test-utils';
-import { compile } from '@ton/blueprint';
+import {compile} from '@ton/blueprint';
 
 describe('Counter', () => {
     let code: Cell;
@@ -27,13 +27,41 @@ describe('Counter', () => {
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
             to: counter.address,
-            deploy: true,
-            success: true,
+            success: false,
+            exitCode: 35
         });
     });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and counter are ready to use
+    it('should update the number ', async () => {
+        const caller = await blockchain.treasury('caller');
+
+        await counter.sendNumber(caller.getSender(), toNano('0.005'), 100n);
+        const counterNum = await counter.getTotal();
+        expect(counterNum).toEqual(110n);
+
+        await counter.sendNumber(caller.getSender(), toNano('0.005'), 50n);
+        const counterNum2 = await counter.getTotal();
+        expect(counterNum2).toEqual(160n);
+
+        await counter.sendNumber(caller.getSender(), toNano('0.005'), 1000n);
+        const counterNum3 = await counter.getTotal();
+        expect(counterNum3).toEqual(1160n);
+
     });
+
+    it('should throw error is not 32 bits ', async () => {
+        const caller = await blockchain.treasury('caller');
+        const result = await counter.sendDeploy(
+            caller.getSender(),
+            toNano('0.01'),
+        );
+
+        expect(result.transactions).toHaveTransaction({
+            from: caller.address,
+            to: counter.address,
+            success: false,
+            exitCode: 35
+        });
+    });
+
 });
