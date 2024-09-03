508a1fb53a25fbbcb51422aa3f80decd4719ad55
libaice <libaice147@gmail.com>
2024-09-03 17:16:09 +0400

    update counter contract

--- scripts/sendNumber.ts
+++ scripts/sendNumber.ts
@@ -4,13 +4,11 @@ import {compile, NetworkProvider, sleep} from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
     const counter = provider.open(Counter.createFromConfig({}, await compile('Counter')));
+    
     console.log('Total:', await counter.getTotal());
-    // await counter.sendNumber(provider.sender(), toNano('0.005'), 100n);
 
-    // for(let i = 0; i < 10; i++) {
-    //     console.log('Total:', await counter.getTotal());
-    //     await sleep(5000);
-    // }
+    // await counter.sendNumber(provider.sender(), toNano('0.005'), 22n);
+
 
 
 }
